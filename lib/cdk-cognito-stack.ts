665403ac1008ba02ce2eb7a29846a2005b83320d
Muhammad Masham <66207787+masham04@users.noreply.github.com>
2021-05-25 15:16:33 +0500

    new

--- lib/cdk-cognito-stack.ts
+++ lib/cdk-cognito-stack.ts
@@ -1,9 +1,32 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as cognito from "@aws-cdk/aws-cognito";
 
 export class CdkCognitoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const authEmailFn = new lambda.Function(this, "authEmailFn", {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset("lambda"),
+    });
+
+    const userPool = new cognito.UserPool(this, "UserPool", {
+      selfSignUpEnabled: true, // Allow users to sign up
+      autoVerify: { email: true }, // Verify email addresses by sending a verification code
+      signInAliases: { email: true }, // Set email as an alias means now you will use email address to authenticate not with username
+      userVerification: {
+        emailSubject: "Verify your email for our awesome app!",
+        emailBody:
+          "Hello {username}, Thanks for signing up to our awesome app! Your verification code is {####}",
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+        smsMessage:
+          "Hello {username}, Thanks for signing up to our awesome app! Your verification code is {####}",
+      }, ///customize email and sms
+      lambdaTriggers: {
+        preSignUp: authEmailFn, ///Trigger before the signup process to userpool
+      },
+    });
   }
 }
